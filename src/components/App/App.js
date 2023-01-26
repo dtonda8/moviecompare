eb107c84fc1d699fc09ce2017aae946764abadf2
David Batonda <99521514+dtonda8@users.noreply.github.com>
2023-01-26 13:29:26 +1100

    Add simple Nav Bar and pop up for movie details

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import AddMovies from '../AddMovies/AddMovies';
+import NavBar from '../NavBar/NavBar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <AddMovies />
     </div>
   );
--- src/components/elements/MovieThumbnails/MovieThumbnails.js
+++ src/components/elements/MovieThumbnails/MovieThumbnails.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './MovieThumbnails.css';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 const BASE_POSTER_URL = 'https://image.tmdb.org/t/p/original'
 
@@ -9,19 +11,55 @@ class MovieThumbnails extends Component {
         const resultsDiv = results.map((result, index) => {
             if (result['poster_path']){
                 const poster = BASE_POSTER_URL + result['poster_path'];
-                let movieTitle = result['title'] 
-                if (!movieTitle) movieTitle = result['name'];
-                const altMsg = movieTitle + ' poster';
+                const id = result['id'];
+                const altMsg = 'Movie poster';
                 return (
                     <div
                         key={index}
                         className='thumbnail'>
                         <img 
+                            id={id + '-img'}
                             className='movie-img'
                             src={poster}    
                             alt={altMsg}>
                         </img>
-                        {movieTitle}
+                            <button 
+                                className='add-to-compare'
+                                id={id + 'add-to-compare-btn'}>
+                                    Add to Compare
+                            </button>
+                            <Popup
+                            trigger={<button 
+                                        className='details'
+                                        id={id + 'details-btn'}>
+                                        Details
+                                    </button>}
+                            modal
+                            nested
+                            >
+                            {close => (
+                            <div className="modal">
+                                <button className="close" onClick={close}>
+                                &times;
+                                </button>
+                                <div className="header"> Modal Title </div>
+                                <div className="content">
+                                {' '}
+                                content
+                                </div>
+                                <div className="actions">
+                                <button
+                                    className="button"
+                                    onClick={() => {
+                                    close();
+                                    }}
+                                >
+                                    Exit
+                                </button>
+                                </div>
+                            </div>
+                            )}
+                            </Popup>
                     </div>
                 )
             }
@@ -31,4 +69,4 @@ class MovieThumbnails extends Component {
 };
 
 
-export default MovieThumbnails
+export default MovieThumbnails;
