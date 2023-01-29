97b8dcf1cd09bcf1f1bbe176e2ded57e140d9c32
David Batonda <99521514+dtonda8@users.noreply.github.com>
2023-01-29 13:45:45 +1100

    Implement Routes for Compare and Search tabs

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import './NavBar.css'
+
 
 class NavBar extends Component {
     render() {
         return (
             <nav>
             <h2>MovieCompare</h2>
-            <h2>Search</h2>
-            <button className="notification">
-                <span>Compare</span>
-                <span className="badge">{this.props.moviesToCompare.length}</span>
-            </button>
+            <Link to='/'>
+                <button>Search</button>
+            </Link>
+            <Link to='/compare'>
+                <button className="notification">
+                    <span>Compare</span>
+                    <span className="badge">{this.props.moviesToCompare.length}</span>
+                </button>
+            </Link>
             <h2>Links to APIs</h2>
             </nav>
         )
