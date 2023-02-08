081caa20bb5774f5e23bc9ee6af30c1e07918e3e
David Batonda <99521514+dtonda8@users.noreply.github.com>
2023-02-09 10:49:02 +1100

    Fix API error

--- src/components/elements/Notifications/Notifications.js
+++ src/components/elements/Notifications/Notifications.js
@@ -1,12 +1,8 @@
 import { Notyf } from 'notyf';
 
-export const generalNotfy = new Notyf({
-    duration: 2000,
-    position: {y: 'top'}
-  });
+export const generalNotfy = new Notyf({ duration: 2000 });
 export const warningNotyf = new Notyf({
     duration: 2000,
-    position: {y: 'top'},
     types: [
     {
         type: 'success',
